0a0caa938a120ed0ecf737d789eb771ae20cc3ba
ektaaaah <155817894+ektaaaah@users.noreply.github.com>
2024-01-06 17:17:09 +0530

    Add files via upload

---
+++ README_TESTS.md
@@ -0,0 +1,120 @@
+## FORGE
+**------Testing------**
+Do first
+```tsc```
+
+Build (may take some time and appear to hang)
+```forge build --via-ir```
+
+Most cases
+```source .env.forge && forge test -vv```
+
+If you need to fork mainnet
+```source .env.forge && forge test --fork-url $MAINNET_RPC_URL -vv```
+
+If you need to fork mainnet, single test contract
+```source .env.forge && forge test --fork-url $MAINNET_RPC_URL -vv --match-path ./src/foundry/test/FXS/FXSDisableVoteTracking.t.sol```
+
+Verbosely test a single contract while forking mainnet
+or ```source .env && forge test --fork-url $MAINNET_RPC_URL -vvvvv --match-path ./src/foundry/test/XYZ/XYZ.t.sol``` for single test verbosity level 5
+
+## Hardhat
+**------Testing------**
+tsc
+cd ./src/hardhat
+npx hardhat compile
+
+ARBITRUM
+npx hardhat test ./test/__ARBITRUM/ComboOracle_KyberSwapElastic-Tests_ARBI.js --vv
+npx hardhat test ./test/__ARBITRUM/CrossChainBridgeBacker_ARBI_AnySwap-Tests.js
+npx hardhat test ./test/__ARBITRUM/FraxCrossChainFarmV2-Tests.js
+npx hardhat test ./test/__ARBITRUM/FraxCrossChainFarmV3_ERC20-Tests.js
+npx hardhat test ./test/__ARBITRUM/FraxCrossChainFarmV4_ERC20-Tests.js --no-compile --full-trace-error
+npx hardhat test ./test/__ARBITRUM/CurveAMO-ARBI-Tests.js
+
+AURORA
+npx hardhat test ./test/__AURORA/CrossChainBridgeBacker_AUR_Rainbow-Tests.js
+
+AVALANCHE
+npx hardhat test ./test/__AVALANCHE/AxialAMO-Tests.js
+npx hardhat test ./test/__AVALANCHE/CrossChainBridgeBacker_AVAX_AnySwap-Tests.js
+npx hardhat test ./test/__AVALANCHE/PangolinLiquidityAMO-Tests.js
+
+BOBA
+Todo
+
+ETHEREUM
+npx hardhat test  ./test/TWAMM_AMO-Tests.js
+npx hardhat test --no-compile ./test/AaveAMO-Tests.js
+npx hardhat test ./test/BAMM/BAMM.js
+npx hardhat test ./test/CPITrackerOracle-Tests.js
+npx hardhat test ./test/ComboOracle_SLP_UniV2_UniV3-Tests.js
+npx hardhat test ./test/CommunalFarm-Tests.js
+npx hardhat test ./test/Convex-AMO.js
+npx hardhat test ./test/CrossChainCanonical-Tests.js
+npx hardhat test ./test/Curve-AMO-V4.js
+npx hardhat test ./test/CurveMetapoolLockerAMO-Tests.js
+npx hardhat test ./test/FPI-FPIS-Tests.js
+npx hardhat test ./test/FPIControllerPool-Tests.js
+npx hardhat test ./test/FXS1559AMO-Tests.js
+npx hardhat test ./test/FraxCrossChainLiquidityTracker-Tests.js
+npx hardhat test ./test/FraxFarmBSC_Dual_V5.js
+npx hardhat test ./test/FraxFarmRageQuitter-Tests.js
+npx hardhat test ./test/Fraxbonds/SlippageAuction.js
+npx hardhat test ./test/Fraxferry/Fraxferry-test.js
+npx hardhat test ./test/FraxferryV2/FerryV2-test.js
+npx hardhat test ./test/FraxGaugeController-Tests.js
+npx hardhat test ./test/FraxLendingAMO.js
+npx hardhat test ./test/FraxLiquidityBridger-Tests.js
+npx hardhat test ./test/FraxMiddlemanGauge-Tests.js
+npx hardhat test ./test/FraxPoolV3-Tests.js
+npx hardhat test ./test/FraxUniV3Farm_Stable_FRAX_DAI-Tests.js
+npx hardhat test ./test/FraxUniV3Farm_Stable_FRAX_USDC-Tests.js
+npx hardhat test ./test/FraxUnifiedFarm_ERC20-Tests.js
+npx hardhat test ./test/FraxUnifiedFarm_KyberSwapElastic-Tests.js
+npx hardhat test ./test/FraxUnifiedFarm_PosRebase-Tests.js
+npx hardhat test ./test/FraxUnifiedFarm_UniV3-Tests.js
+npx hardhat test ./test/Fraxswap/fraxswap-twamm-test.js
+npx hardhat test ./test/Fraxswap/fraxswap-uniV2-test.js
+npx hardhat test ./test/FrxETH/FrxETHMiniRouter-Tests.js
+npx hardhat test ./test/Governance_Slap_2.js
+npx hardhat test ./test/IFraxGaugeFXSRewardsDistributor-Tests.js
+npx hardhat test ./test/InvestorAMO_V3-Tests.js
+npx hardhat test ./test/MIM-Convex-AMO-Tests.js
+npx hardhat test ./test/MSIGHelper-Tests.js
+npx hardhat test ./test/MigratableFarmBSC_FRAX_FXS-Tests.js
+npx hardhat test ./test/OHM-AMO.js
+npx hardhat test ./test/PIDController-Tests.js
+npx hardhat test ./test/Rari-AMO-Tests.js
+npx hardhat test ./test/StakeDAO-AMO.js
+npx hardhat test ./test/TokenTrackerV2-Tests.js
+npx hardhat test ./test/UniV3LiquidityAMO-Tests.js
+npx hardhat test ./test/UniV3LiquidityAMO_V2-Tests.js
+npx hardhat test ./test/UniV3TWAPOracle-Tests.js
+npx hardhat test ./test/openzeppelin/ERC20.test.js
+npx hardhat test ./test/veFPIS-Tests.js
+npx hardhat test ./test/veFPISYieldDistributorV5-Tests.js
+npx hardhat test ./test/veFXSYieldDistributorV4-Tests.js
+
+FANTOM
+npx hardhat test ./test/__FANTOM/CrossChainBridgeBacker_FTM_AnySwap-Tests.js
+npx hardhat test ./test/__FANTOM/CurveAMO-FTM-Tests.js
+npx hardhat test ./test/__FANTOM/FPIControllerPool-Tests.js
+npx hardhat test ./test/__FANTOM/ScreamAMO-Tests.js
+npx hardhat test ./test/__FANTOM/SpiritSwapAMO-Tests.js
+
+HARMONY
+npx hardhat test ./test/__HARMONY/CrossChainBridgeBacker_HARM_Horizon-Tests.js
+
+MOONBEAM
+npx hardhat test ./test/__MOONBEAM/CrossChainBridgeBacker_MNBM_Nomad-Tests.js
+
+MOONRIVER
+npx hardhat test ./test/__MOONRIVER/CCFrax1to1AMM-Tests.js
+
+OPTIMISM
+npx hardhat test ./test/__OPTIMISM/CrossChainBridgeBacker_OPTI_Celer-Tests.js
+
+POLYGON
+npx hardhat test ./test/__POLYGON/CrossChainBridgeBacker_POLY_MaticBridge-Tests.js
+npx hardhat test ./test/__POLYGON/SushiSwapLiquidityAMO_POLY-Tests.js
---
+++ hardhat.config.js
@@ -0,0 +1,524 @@
+const path = require('path');
+const envPath = path.join(__dirname, '../../.env');
+require('dotenv').config({ path: envPath });
+
+require('hardhat-deploy');
+require('hardhat-contract-sizer');
+require('hardhat-gas-reporter');
+require("hardhat-tracer");
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-vyper");
+require('hardhat-spdx-license-identifier');
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+		hardhat: {
+			// chainId: 1337, // Ethereum (alternate / frontend testing)
+			forking: {
+				// url: `${process.env.ARBITRUM_NETWORK_ENDPOINT}`, // Arbitrum
+				// url: `${process.env.AURORA_NETWORK_ENDPOINT}`, // Aurora
+				// url: `${process.env.AVALANCHE_FORKING_NETWORK_ENDPOINT}`, // Avalanche
+				// url: `${process.env.BOBA_NETWORK_ENDPOINT}`, // Boba
+				// url: `${process.env.BSC_NETWORK_ENDPOINT}`, // BSC
+				url: `${process.env.ETHEREUM_NETWORK_ENDPOINT}`, // Ethereum
+				// url: `${process.env.EVMOS_NETWORK_ENDPOINT}`, // Evmos
+				// url: `${process.env.FANTOM_FORKING_NETWORK_ENDPOINT}`, // Fantom
+				// url: `${process.env.FUSE_NETWORK_ENDPOINT}`, // Fuse
+				// url: `${process.env.HARMONY_NETWORK_ENDPOINT}`, // Harmony
+				// url: `${process.env.MOONBEAM_NETWORK_ENDPOINT}`, // Moonbeam
+				// url: `${process.env.MOONRIVER_NETWORK_ENDPOINT}`, // Moonriver
+				// url: `${process.env.OPTIMISM_NETWORK_ENDPOINT}`, // Optimism
+				// url: `${process.env.POLYGON_NETWORK_ENDPOINT}`, // Polygon
+				// url: `${process.env.ZKSYNC_NETWORK_ENDPOINT}`, // zkSync
+				// zksync: true
+
+				// TESTING (npx hardhat node --hostname 0.0.0.0)
+				// Also see src/hardhat/justin-scripts/instructions.txt
+				// url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`, // Ethereum (alternate)
+
+				blockNumber: 149451388 // Pin the block to allow caching
+				
+			},
+			accounts: {
+				mnemonic: process.env.ROPSTEN_HARDHAT_PHRASE
+			},
+			// hardfork: 'shanghai'
+		},
+		arbitrum: {
+			url: process.env.ARBITRUM_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.ARBITRUM_MNEMONIC_PHRASE
+			},
+			chainId: 42161,
+			gas: "auto",
+			gasPrice: 150000000, // 0.15 Gwei
+			gasMultiplier: 1.2
+		},
+		aurora: {
+			url: process.env.AURORA_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.AURORA_MNEMONIC_PHRASE
+			},
+			chainId: 1313161554,
+			gas: "auto",
+			gasPrice: 3500000000, // 3.5 Gwei
+			gasMultiplier: 1.2
+		},
+		avalanche: {
+			url: process.env.AVALANCHE_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.AVALANCHE_MNEMONIC_PHRASE
+			},
+			chainId: 43114,
+			gas: "auto",
+			gasPrice: 75000000000, // 75 Gwei
+			gasMultiplier: 1.2
+		},
+		boba: {
+			url: process.env.BOBA_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.BOBA_MNEMONIC_PHRASE
+			},
+			chainId: 288,
+			gas: "auto",
+			gasPrice: 80000000000, // 80 Gwei
+			gasMultiplier: 1.2
+		},
+		bsc: {
+			url: process.env.BSC_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.BSC_MNEMONIC_PHRASE
+			},
+			chainId: 56,
+			gas: "auto",
+			gasPrice: 6000000000, // 6 Gwei
+			gasMultiplier: 1.2
+		},
+		ethereum: {
+			url:`${process.env.ETHEREUM_NETWORK_ENDPOINT}`,
+			accounts: {
+				mnemonic: process.env.MNEMONIC_PHRASE,
+			},
+			chainId: 1,
+			gas: "auto",
+			gasPrice: 50000000000, // 50 Gwei
+			gasMultiplier: 1.2,
+		},
+		evmos: {
+			url: process.env.EVMOS_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.EVMOS_MNEMONIC_PHRASE
+			},
+			chainId: 9001,
+			gas: "auto",
+			gasPrice: 10000000000, // 10 Gwei
+			gasMultiplier: 1.2
+		},
+		fantom: {
+			url: process.env.FANTOM_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.FANTOM_MNEMONIC_PHRASE
+			},
+			chainId: 250,
+			gas: "auto",
+			gasPrice: 750000000000, // 750 Gwei
+			gasMultiplier: 1.2
+		},
+		fraxchain_devnet_l1: {
+			url:`${process.env.FRAXCHAIN_DEVNET_L1_NETWORK_ENDPOINT}`,
+			accounts: {
+				mnemonic: process.env.MNEMONIC_PHRASE,
+			},
+			httpHeaders: {
+				"Cookie": `${process.env.FRAXCHAIN_DEVNET_COOKIE}`
+			},
+			chainId: 2520,
+			gas: "auto",
+			gasPrice: 2000000000, // 2 Gwei
+			gasMultiplier: 1.2,
+		},
+		fraxchain_devnet_l2: {
+			url:`${process.env.FRAXCHAIN_DEVNET_L2_NETWORK_ENDPOINT}`,
+			accounts: {
+				mnemonic: process.env.MNEMONIC_PHRASE,
+			},
+			httpHeaders: {
+				"Cookie": `${process.env.FRAXCHAIN_DEVNET_COOKIE}`
+			},
+			chainId: 2521,
+			gas: "auto",
+			gasPrice: 2000000000, // 2 Gwei
+			gasMultiplier: 1.2,
+		},
+		// fuse: {
+		// 	url: process.env.FUSE_NETWORK_ENDPOINT,
+		// 	accounts: {
+		// 		mnemonic: process.env.FUSE_MNEMONIC_PHRASE
+		// 	},
+		// 	chainId: 122,
+		// 	gas: "auto",
+		// 	gasPrice: 5000000000, // 5 Gwei
+		// 	gasMultiplier: 1.2
+		// },
+		harmony: {
+			url: process.env.HARMONY_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.HARMONY_MNEMONIC_PHRASE
+			},
+			chainId: 1666600000,
+			gas: "auto",
+			gasPrice: 50000000000, // 50 Gwei
+			gasMultiplier: 1.2
+		},
+		holesky: {
+			url: process.env.HOLESKY_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.HOLESKY_MNEMONIC_PHRASE
+			},
+			chainId: 17000,
+			gas: "auto",
+			gasPrice: 10000000000, // 10 Gwei
+			gasMultiplier: 1.2
+		},
+		moonbeam: {
+			url: process.env.MOONBEAM_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.MOONBEAM_MNEMONIC_PHRASE
+			},
+			chainId: 1284,
+			gas: "auto",
+			gasPrice: 150000000000, // 150 Gwei
+			gasMultiplier: 1.2
+		},
+		moonriver: {
+			url: process.env.MOONRIVER_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.MOONRIVER_MNEMONIC_PHRASE
+			},
+			chainId: 1285,
+			gas: "auto",
+			gasPrice: 3000000000, // 3 Gwei
+			gasMultiplier: 1.2
+		},
+		optimism: {
+			url: process.env.OPTIMISM_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.OPTIMISM_MNEMONIC_PHRASE
+			},
+			chainId: 10,
+			gas: "auto",
+			gasPrice: 25000000, // 0.025 Gwei
+			gasMultiplier: 1.2
+		},
+		polygon: {
+			url: `${process.env.POLYGON_NETWORK_ENDPOINT}`,
+			accounts: {
+				mnemonic: process.env.POLYGON_MNEMONIC_PHRASE
+			},
+			chainId: 137,
+			gas: "auto",
+			gasPrice: 50000000000, // 50 Gwei
+			gasMultiplier: 1.2
+		},
+		polygon_zkevm: {
+			url: `${process.env.POLYGON_ZKEVM_NETWORK_ENDPOINT}`,
+			accounts: {
+				mnemonic: process.env.POLYGON_ZKEVM_MNEMONIC_PHRASE
+			},
+			chainId: 1101,
+			gas: "auto",
+			gasPrice: 10000000000, // 10 Gwei
+			gasMultiplier: 1.2
+		},
+		polygon_mumbai: {
+			url: `${process.env.POLYGON_MUMBAI_NETWORK_ENDPOINT}`,
+			accounts: {
+				mnemonic: process.env.ROPSTEN_HARDHAT_PHRASE
+			},
+			chainId: 80001,
+			gas: "auto",
+			gasPrice: 4000000000, // 4 Gwei
+			gasMultiplier: 1.2
+		},
+		ropsten: {
+			url:`https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+			accounts: {
+				mnemonic: process.env.ROPSTEN_HARDHAT_PHRASE
+			},
+			chainId: 3,
+			gas: "auto",     
+			gasPrice: "auto", 
+			gasMultiplier: 1.2
+		},
+		rinkeby: {
+			url:`https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+			accounts: {
+				mnemonic: process.env.MNEMONIC_PHRASE
+			},
+			chainId: 4,
+			gas: "auto",
+			gasPrice: "auto",
+			gasMultiplier: 1.2
+		},
+		zksync: {
+			url: process.env.ZKSYNC_NETWORK_ENDPOINT,
+			accounts: {
+				mnemonic: process.env.ZKSYNC_MNEMONIC_PHRASE
+			},
+			chainId: 324,
+			gas: "auto",
+			gasPrice: "auto",
+			// gasPrice: 3000000000, // 3 Gwei
+			gasMultiplier: 1.2,
+			zksync: true
+		},
+    },
+	solidity: {
+		compilers: [
+			{
+				version: "0.5.17",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					},
+				  }
+			},
+			{
+				version: "0.6.11",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.6.12",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.7.6",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			// {
+			// 	version: "0.8.0",
+			// 	settings: {
+			// 		optimizer: {
+			// 			enabled: true,
+			// 			runs: 100000
+			// 		}
+			// 	  }
+			// },
+			// {
+			// 	version: "0.8.2",
+			// 	settings: {
+			// 		optimizer: {
+			// 			enabled: true,
+			// 			runs: 100000
+			// 		}
+			// 	  }
+			// },
+			{
+				version: "0.8.4",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.8.6",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.8.10",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.8.13",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.8.15",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.8.16",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				  }
+			},
+			{
+				version: "0.8.15",
+				settings: {
+					// viaIR: true,
+					// optimizer: {
+					// 	enabled: true,
+					// 	runs: 200000,
+					// 	details: {
+					// 		orderLiterals: true,
+					// 		deduplicate: true,
+					// 		cse: true,
+					// 		constantOptimizer: true,
+					// 		yul: true,
+					// 		yulDetails: {
+					// 			stackAllocation: true
+					// 		}
+					// 	},
+					// }
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				}
+			},
+			{
+				version: "0.8.19",
+				settings: {
+					// viaIR: true,
+					// optimizer: {
+					// 	enabled: true,
+					// 	runs: 200000,
+					// 	details: {
+					// 		orderLiterals: true,
+					// 		deduplicate: true,
+					// 		cse: true,
+					// 		constantOptimizer: true,
+					// 		yul: true,
+					// 		yulDetails: {
+					// 			stackAllocation: true
+					// 		}
+					// 	},
+					// }
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				}
+			},
+			{
+				version: "0.8.23",
+				settings: {
+					// viaIR: true,
+					// optimizer: {
+					// 	enabled: true,
+					// 	runs: 200000,
+					// 	details: {
+					// 		orderLiterals: true,
+					// 		deduplicate: true,
+					// 		cse: true,
+					// 		constantOptimizer: true,
+					// 		yul: true,
+					// 		yulDetails: {
+					// 			stackAllocation: true
+					// 		}
+					// 	},
+					// }
+					optimizer: {
+						enabled: true,
+						runs: 100000
+					}
+				}
+			},
+		],
+	},
+	zksolc: {
+		version: "1.3.9",
+		compilerSource: "binary",
+		settings: {
+			// forceEvmla: true // optional. Falls back to EVM legacy assembly if there is a bug with Yul
+		}
+	},
+    paths: {
+      sources: "./contracts",
+      tests: "./test",
+      cache: "./cache",
+      artifacts: "./artifacts"
+    },
+    mocha: {
+      timeout: 50000000
+	},
+	etherscan: {
+		// apiKey: process.env.BSCSCAN_API_KEY // BSC
+		apiKey: process.env.ETHERSCAN_API_KEY, // ETH Mainnet
+		// apiKey: process.env.FTMSCAN_API_KEY // Fantom
+		// apiKey: process.env.OPTIMISM_API_KEY, // Optimism
+		// apiKey: process.env.POLYGONSCAN_API_KEY // Polygon
+	},
+	gasReporter: {
+		currency: 'USD',
+		gasPrice: 80,
+		enabled: true,
+		maxMethodDiff: 10,
+	},
+	contractSizer: {
+		alphaSort: true,
+		runOnCompile: true,
+		disambiguatePaths: false,
+	},
+    vyper: {
+		// version: "0.2.15"
+		// version: "0.2.16"
+		version: "0.3.7"
+    }
+};
+
